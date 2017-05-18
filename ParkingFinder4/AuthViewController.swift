18a07ab1a0354f4601d6c7be4d2a3b120e5434d9
Etudiant <Etudiant@iMac-de-Etudiant-24.local>
2017-05-18 12:30:37 +0200

    first push

---
+++ ParkingFinder4/AuthViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AuthViewController.swift
+//  ParkingFinder4
+//
+//  Created by Etudiant on 18/05/2017.
+//  Copyright © 2017 esgi. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class AuthViewController: UIViewController {
+
+    @IBOutlet var login: UITextField!
+    @IBOutlet var password: UITextField!
+    @IBOutlet var connect: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-ParkingFinder4/Pods-ParkingFinder4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ParkingFinder4 : NSObject
+@end
+@implementation PodsDummy_Pods_ParkingFinder4
+@end
